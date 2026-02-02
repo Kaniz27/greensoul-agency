b829ac8435091aa46ad6290d6da0fec7ae5ff9c4
unknown <you@example.com>
2026-02-02 15:58:24 +0600

    any code add

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ import BlogPage from './pages/Blog';
 import Contact from './pages/Contact';
 import Auth from './pages/Auth';
 import { User } from './types';
-import ServicesPage from './pages/ServicesPage';
+
 
 // ScrollToTop component to reset scroll position on route change
 const ScrollToTop = () => {
@@ -54,7 +54,7 @@ const App: React.FC = () => {
             <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
             <Route path="/services/:slug" element={<Services />} />
-            <Route path="/services" element={<ServicesPage />} />
+          
 
             <Route path="/case-studies" element={<CaseStudies />} />
             <Route path="/blog" element={<BlogPage />} />
